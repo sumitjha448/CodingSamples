71bb039003bdfbf1704c8f1281d0218eecb8c0ed
Sumit Kumar <sumit.k.jha1@gmail.com>
2019-08-17 19:16:10 +0530

    Update IterativePostOrderTraversal.java

--- IterativePostOrderTraversal.java
+++ IterativePostOrderTraversal.java
@@ -4,6 +4,8 @@ public ArrayList<Integer> postorderTraversal(TreeNode A) {
         TreeNode temp = A;
         Stack<TreeNode> stack = new Stack<>();
         while(true){
+                // if the root has a right child first push that and then the root
+                // make root as root.left
             while(root != null){
                 if(root.right != null)
                 stack.push(root.right);
@@ -13,6 +15,15 @@ public ArrayList<Integer> postorderTraversal(TreeNode A) {
             if(!stack.isEmpty()){
                 temp = stack.pop();
                 if(temp.right != null){
+                        
+                     /*
+                     this is very important -
+                     So if we see that the root has a right child and it is at top of the stack
+                     it means its right child is not processed. So remove the right child from the stack so that next time
+                     it doesnt iterate for the same right child. Also push the root since we will
+                     process the root once we have processed the right subtrees. and  then iterate
+                     for the right child
+                     */
                     if(!stack.isEmpty() && temp.right == stack.peek()){
                         root = stack.pop();
                         stack.push(temp);
